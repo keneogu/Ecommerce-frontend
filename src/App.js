e57947455985ae684c56cd7f4605f4f7dc757228
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-06 15:53:34 +0100

    Add navigate to notfound when clicked unrecognized link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import './App.css';
 import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
 import About from "./containers/About/About";
 import Home from "./containers/Home/Home";
 
@@ -14,6 +15,8 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/notfound" replace />} />
         </Routes>
       </Router>
     </div>
