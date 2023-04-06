667492e85c79d848c9d619fd1adb5df3c1d8afc9
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-06 15:51:13 +0100

    Add shoppingcart icon and link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
 	return (
-		<div>
+		<div className='flex justify-between text-white bg-black h-20 items-center px-4'>
 			<div className='link'>
-			<Link to="/">Kenzshop</Link>
+				<Link to="/"><h3>KenzShop</h3></Link>
 			</div>
-			<nav>
-				<Link to="/about">About</Link>
+			<nav className='flex flex-row items-center'>
+				<Link to="/about"><p>About</p></Link>
+				<Link to="/cart">
+					<div className='flex'>
+						<p><FaShoppingCart className='text-orange-700' /></p>
+						<span>3</span>
+					</div>
+				</Link>
 			</nav>
 		</div>
 	)
