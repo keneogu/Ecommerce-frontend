8c74bbd84b816acfb36067b342a86397f401352a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-12 13:15:04 +0100

    fetch api using redux thunk and create product Slice

---
+++ src/ProductSlice.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+	items: [],
+	status: null
+}
+
+export const fetchProducts = createAsyncThunk(
+	"products/fetchproducts",
+	async () => {
+		const response =	await axios.get("http://localhost:5000/products")
+		return response?.data
+	}
+)
+
+const ProductSlice = createSlice({
+	name: "products",
+	initialState,
+	reducers: {},
+	extraReducers: {
+		[fetchProducts.pending]: (state, action) => {
+			state.status = "pending"
+		},
+		[fetchProducts.fulfilled]: (state, action) => {
+			state.status = "success"
+			state.items = action.payload
+		},
+		[fetchProducts.rejected]: (state, action) => {
+			state.status = "rejected"
+		},
+	}
+});
+
+export default ProductSlice.reducer
