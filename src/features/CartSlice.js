c8fdbeec488096d10d07f4bc24c5dbb045765372
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-13 11:49:42 +0100

    Add react-toastify for easy notification messages.

--- src/features/CartSlice.js
+++ src/features/CartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify"
 
 const initialState = {
-	cartItems: [],
+	cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
 	cartTotalQuantity: 0,
 	cartTotalAmount: 0,
 };
@@ -13,12 +14,20 @@ const cartSlice = createSlice({
 		addToCart(state,action){
 			const itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id)
 			if(itemIndex >= 0) {
-				state.cartItems[itemIndex].cartQuantity += 1
+				state.cartItems[itemIndex].cartQuantity += 1;
+				toast.info("increased product quantity", {
+					position: "bottom-left"
+				})
 			} else {
 				const tempProduct = {...action.payload, cartQuantity: 1};
 				state.cartItems.push(tempProduct);
+				state.cartItems[itemIndex].cartQuantity += 1;
+				toast.success("new product added to cart", {
+					position: "bottom-left"
+				})
 			}
 			
+			localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
 		},
 	},
 });
