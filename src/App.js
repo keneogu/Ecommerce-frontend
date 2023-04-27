ce44c4c0809bf109820ae105cd4d5b7bd6524cba
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-27 16:24:32 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Cart from "./containers/Cart/Cart";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 import About from "./containers/About/About";
 import Home from "./containers/Home/Home";
@@ -21,6 +22,7 @@ function App() {
           <Route path="/notfound" element={<NotFound />} />
           <Route path="*" element={<Navigate to="/notfound" replace />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div>
+			<footer>
+				<p className='text-cen'>Kenz-Shopping Cart @2023, All rights are Reserved by me</p>
+			</footer>
+		</div>
+	)
+}
+
+export default Footer
