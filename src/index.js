c49c356cad26e8f114e7d2e19baf5b72bc1c8747
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-28 21:53:17 +0100

    Add isLoading and dispatch fetchProduct and

--- src/index.js
+++ src/index.js
@@ -2,25 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-
-import productReducer, { fetchProducts } from "./features/ProductSlice";
-import { productsApi } from './features/ProductApi';
-import cartReducer, { getTotals} from './features/CartSlice';
-
-const store = configureStore({
-  reducer:{
-    products: productReducer,
-    cart: cartReducer,
-    [productsApi.reducerPath]: productsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
-});
-
-store.dispatch(fetchProducts())
-store.dispatch(getTotals())
+import {store} from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
