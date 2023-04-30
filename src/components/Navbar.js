fb10f93627bd82239d2047d45040d0f1fe5c9248
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-30 16:57:11 +0100

    Add search input and button to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaShoppingCart } from "react-icons/fa";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
 	const {cartTotalQuantity} = useSelector(state => state.cart);
 
 	return (
-		<div className='flex justify-between text-white bg-black h-20 items-center px-4'>
+		<div className='flex justify-between text-white bg-kenz-400 h-14 items-center px-4'>
 			<div className='link'>
 				<Link to="/"><h3>KenzShop</h3></Link>
 			</div>
-			<nav className='flex flex-row items-center'>
-				<Link to="/about"><p>About</p></Link>
+
+			<div className='bg-white border-kenz-100 border-4 rounded-md px-2 py-2 my-3'>
+				<input type='text' className='bg-lime-100' placeholder='Enter Product Name'/>
+				<button><FaSearch className='text-black w-6 h-6 bg-gray-100' /></button>
+			</div>
+			
+			<nav className='flex flex-row items-center px-2'>
+				<Link to="/about"><p>Login</p></Link>
 				<Link to="/cart">
 					<div className='flex'>
 						<p><FaShoppingCart className='text-orange-700' /></p>
