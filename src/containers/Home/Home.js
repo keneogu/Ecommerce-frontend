9c66a644552ddf882efea83128c718576b357526
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-01 17:28:42 +0100

    Add pagination functions to display certain product/page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from "../../features/ProductSlice";
 import ProductCard from './ProductCard';
 import Head from '../../components/layout/Head'
 import Loader from '../../components/layout/Loader';
+import Pagination from 'react-js-pagination'
 
 const Home = () => {
-	const { products } = useSelector(state => state.products.products)
+	const [currentPage, setCurrentPage] = useState(1)
+	const { products, productCount, perPage } = useSelector(state => state.products.products)
 	const { isLoading } = useSelector(state => state.products)
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		dispatch(fetchProducts())
-	}, [dispatch])
+		dispatch(fetchProducts(currentPage))
+	}, [dispatch, currentPage])
 
+	function setCurrentPageNum(pageNumber) {
+		setCurrentPage(pageNumber)
+	}
 
 	return (
 		<>
@@ -24,10 +29,22 @@ const Home = () => {
 
 						<h4 className='text-gray-400 text-xl font-bold capitalize'>Latest Product</h4>
 						<div className="container mx-auto">
-			 {products?.map(product => (
-							<ProductCard key={product._id} product={product} />
-				))}
+							{products?.map(product => (
+											<ProductCard key={product._id} product={product} />
+								))}
 						</div>
+
+						{perPage <= productCount && (
+							<div className='flex justify-center mt-5'>
+								<Pagination
+									activePage={currentPage}
+									itemsCountPerPage={perPage}
+									totalItemsCount={productCount}
+									pageRangeDisplayed={5}
+									onChange={setCurrentPageNum}
+								/>
+							</div>
+						)}
 					</div>
 				</>
 			)}
