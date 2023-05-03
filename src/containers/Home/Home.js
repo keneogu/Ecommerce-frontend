e618e0fb524657810737c422bbdf06c7f17f9dc2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-03 12:57:36 +0100

    Dispatch fetchProducts when search input is added

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,20 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchProducts } from "../../features/ProductSlice";
+import { fetchProducts} from "../../features/ProductSlice";
+import {SearchContext} from "../../components/context/SearchContext";
+
+// import { useParams } from 'react-router-dom';
 import ProductCard from './ProductCard';
 import Head from '../../components/layout/Head'
 import Loader from '../../components/layout/Loader';
 import Pagination from 'react-js-pagination'
 
 const Home = () => {
+	// const { search } = useParams()
+	const { search } = useContext(SearchContext);
+	// const search = useSelector(state => state.products.search)
 	const [currentPage, setCurrentPage] = useState(1)
 	const { products, productCount, perPage } = useSelector(state => state.products.products)
 	const { isLoading } = useSelector(state => state.products)
 	const dispatch = useDispatch();
 
+	// const search = match.params.search
+
 	useEffect(() => {
-		dispatch(fetchProducts(currentPage))
-	}, [dispatch, currentPage])
+		// setSearch(search)
+		dispatch(fetchProducts(search, currentPage))
+	}, [dispatch, search, currentPage]);
 
 	function setCurrentPageNum(pageNumber) {
 		setCurrentPage(pageNumber)
@@ -30,8 +39,8 @@ const Home = () => {
 						<h4 className='text-gray-400 text-xl font-bold capitalize'>Latest Product</h4>
 						<div className="container mx-auto">
 							{products?.map(product => (
-											<ProductCard key={product._id} product={product} />
-								))}
+								<ProductCard key={product._id} product={product} />
+							))}
 						</div>
 
 						{perPage <= productCount && (
