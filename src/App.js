f430b9772cfa7a987006b500d8f23a39a1f877cd
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-03 12:53:13 +0100

    use useSearchParams and contextapi to share props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from "react-router-dom";
+import {useSearchParams } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -8,22 +10,27 @@ import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 import Home from "./containers/Home/Home";
 import ProductDetail from "./containers/ProductDetail";
+import {SearchContext} from "./components/context/SearchContext";
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+	const [search, setSearch] = useState(searchParams.get('search'));
+  
   return (
     <div className="App">
-      <Router>
-      <ToastContainer />
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/notfound" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/notfound" replace />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <SearchContext.Provider value={{search, setSearch, searchParams, setSearchParams}}>
+        <ToastContainer />
+            <Navbar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/search/:search" element={<Home />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="*" element={<Navigate to="/notfound" replace />} />
+            </Routes>
+              <Footer />
+      </SearchContext.Provider>
     </div>
   );
 }
