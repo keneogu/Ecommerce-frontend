96102272718774f590ea4d0e61805812ee2b3e46
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-05 13:19:21 +0100

    Removed context api but used redux state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Routes, Route, Navigate } from "react-router-dom";
-import {useSearchParams } from "react-router-dom";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -10,27 +9,24 @@ import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 import Home from "./containers/Home/Home";
 import ProductDetail from "./containers/ProductDetail";
-import {SearchContext} from "./components/context/SearchContext";
 
 function App() {
-  const [searchParams, setSearchParams] = useSearchParams();
-	const [search, setSearch] = useState(searchParams.get('search'));
   
   return (
     <div className="App">
-      <SearchContext.Provider value={{search, setSearch, searchParams, setSearchParams}}>
+      <Router>
         <ToastContainer />
-            <Navbar />
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/search/:search" element={<Home />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="*" element={<Navigate to="/notfound" replace />} />
-            </Routes>
-              <Footer />
-      </SearchContext.Provider>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/search/:search" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/notfound" replace />} />
+          </Routes>
+          <Footer />
+      </Router>
     </div>
   );
 }
