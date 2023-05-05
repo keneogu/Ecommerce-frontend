d6d0b75fe14c722aa9c0837e0e58e6c30acc1054
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-05 13:28:40 +0100

    Add useRef and Add handleSearch function

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,34 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from "react-redux"
 import { FaSearch } from "react-icons/fa";
 
-const Search = ({ navigate, setSearchParams, search, setSearch }) => {
-
-	const handleChange = (e) => {
-		const newValue = e.target.value
-		setSearch(newValue);
-		setSearchParams({
-			search: e.target.value
-		})
-	}
+const Search = ({ navigate, searchItems}) => {
+	const nameInput = useRef(null);
+	const dispatch = useDispatch();
 
 	const handleSearch = (e) => {
 		e.preventDefault();
-		if (search) {
-			navigate(`search/${search}`)
-			setSearch("");
-		} else {
-			navigate('/')
+		let searchInput = nameInput.current.value
+		if(searchInput) {
+			dispatch(searchItems(searchInput))
+			navigate(`search/${searchInput}`)
+			nameInput.current.value = ""
+			console.log(searchInput)
 		}
 	}
+
 
 	return (
 		<form onSubmit={handleSearch}>
 			<div className='bg-white border-kenz-100 border-4 rounded-md px-2 py-2 my-3'>
-				<input type='text' className='bg-lime-100' onChange={handleChange} value={search || ""} placeholder='Enter Product Name' />
+				<input type='text' className='bg-lime-100' ref={nameInput} placeholder='Enter Product Name' />
 				<button><FaSearch className='text-black w-6 h-6 bg-gray-100' /></button>
 			</div>
 		</form>
 	)
 }
 
-export default Search
+export default Search;
