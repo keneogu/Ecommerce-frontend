6319ce009b85b66770001687e98e69532ce031a5
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-09 12:44:24 +0100

    Dispatch the current logged in user and update its state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 import Cart from "./containers/Cart/Cart";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 import Home from "./containers/Home/Home";
 import ProductDetail from "./containers/ProductDetail";
-import Login from './containers/User/Login';
-import Register from './containers/User/Register';
+import Login from "./containers/User/Login";
+import Register from "./containers/User/Register";
+import { loadUser } from "./features/UserSlice";
+import { store } from "./store";
 
 function App() {
-  
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <ToastContainer />
-          <Navbar />
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/search/:search" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/notfound" replace />} />
-          </Routes>
-          <Footer />
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/search/:search" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/notfound" replace />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
