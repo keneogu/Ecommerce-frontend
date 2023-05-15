3fc5d659c49c80bbeb448852ff37025f75ec5484
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-15 17:01:32 +0100

    Add disabled when the cartquantity = stock

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 import { getTotals, removeCart, decreaseCart, clearCart, addToCart } from "../../features/CartSlice";
 
 const Cart = () => {
+	const { cartItems } = useSelector((state) => state.cart);
 	const cart = useSelector((state) => state.cart);
 	const dispatch = useDispatch()
 
 	useEffect(() => {
 		dispatch(getTotals())
-	},[cart, dispatch])
+	},[cartItems, dispatch])
 
 	const handleRemoveCart = (cartItem) => {
 		dispatch(removeCart(cartItem));
@@ -30,17 +31,18 @@ const Cart = () => {
 	return (
 		<div>
 			<h2>Shopping Cart</h2>
-			{cart.cartItems.length === 0 ? (
+			{cartItems.length === 0 ? (
 				<div>
 					<p>Your cart is currently empty</p>
 					<div>
 						<Link to="/">
-							<span>Start Shopping</span>
+							<span>Continue Shopping</span>
 						</Link>
 					</div>
 				</div>
 			) : (
 				<div>
+					<h2>{cartItems.length}</h2>
 					<div>
 						<h3>Product</h3>
 						<h3>Price</h3>
@@ -48,21 +50,21 @@ const Cart = () => {
 						<h3>Total</h3>
 					</div>
 					<div className="cart-items">
-						{cart.cartItems?.map(cartItem => (
+						{cartItems?.map(cartItem => (
 							<div className="cart-item" key={cartItem._id}>
 								<div className="cart-product">
 									<img src={cartItem.image} alt={cartItem.name} />
 									<div>
-										<h3>{cartItem.name}</h3>
+										<Link to={`/product/${cartItem._id}`}>{cartItem.name}</Link>
 										<p>{cartItem.desc}</p>
 										<button onClick={() => handleRemoveCart(cartItem)}>Remove</button>
 									</div>
 								</div>
 								<div className="cart-product-price">${cartItem.price}</div>
 								<div className="cart-product-quantity">
-									<button onClick={() => handleDecreaseCart(cartItem)}>-</button>
+									<button disabled={cartItem.cartQuantity === 1} onClick={() => handleDecreaseCart(cartItem)} className={cartItem.cartQuantity === 1 ? 'text-gray-400' : 'text-inherit'}>-</button>
 									<div className="count">{cartItem.cartQuantity}</div>
-									<button onClick={() => handleAddToCart(cartItem)}>+</button>
+									<button disabled={cartItem.stock === cartItem.cartQuantity} onClick={() => handleAddToCart(cartItem)} className={cartItem.stock === cartItem.cartQuantity ? 'text-gray-400' : 'text-inherit'}>+</button>
 								</div>
 								<div className="cart-product-total-price">
 									${cartItem.price * cartItem.cartQuantity}
