93ac4067e58dd37b205d1e7508443f28461ef120
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-26 21:43:47 +0100

    Add sidebar components and page skeleton

--- src/admin/Sidebar.js
+++ src/admin/Sidebar.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaTachometerAlt, FaShoppingBasket, FaUserAlt } from "react-icons/fa";
 
 const Sidebar = () => {
+	const [open, setOpen] = useState(false);
 	return (
 		<div>
-		 Sidebar
+			<nav>
+				<ul>
+					<li>
+						<Link to="/dashboard">
+							<FaTachometerAlt />
+							Dashboard
+						</Link>
+					</li>
+					<li>	
+						<label tabIndex={0} onClick={() => setOpen(!open)} className="btn m-1">Click</label>
+						<ul tabIndex={0} className={open ? "block" : "hidden"}>
+							<li><Link to="/admin/products">All Product</Link></li>
+							<li><Link to="/admin/product">Create Product</Link></li>
+						</ul>
+					</li>
+					<li>
+						<Link to="/admin/orders">
+							<FaShoppingBasket />
+							Orders
+						</Link>
+					</li>
+					<li>
+						<Link to="/admin/users">
+							<FaUserAlt />
+							Users
+						</Link>
+					</li>
+					<li>
+						<Link to="/admin/reviews">
+							<FaUserAlt />
+							Reviews
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	)
 }
 
-export default Sidebar
+export default Sidebar;
