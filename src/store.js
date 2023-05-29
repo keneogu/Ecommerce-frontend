8f85c40dc3c37e60e60935fe92d2e77ae290d5a1
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-29 23:58:43 +0100

    Add admin reducer to store

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import userReducer from "./features/UserSlice";
 import orderReducer, {myOrders} from "./features/OrderSlice";
 import { productsApi } from './features/ProductApi';
 import cartReducer, { getTotals} from './features/CartSlice';
+import adminReducer from './features/AdminSlice';
 
 export const store = configureStore({
   reducer:{
@@ -11,6 +12,7 @@ export const store = configureStore({
     user: userReducer,
     cart: cartReducer,
     order: orderReducer,
+    admin: adminReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
