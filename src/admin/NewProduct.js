39ae87b1c13a62bf935fd9c38fab38971b48c54e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-01 23:57:48 +0100

    Remove the error of stock not updating

--- src/admin/NewProduct.js
+++ src/admin/NewProduct.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { createProducts } from "../features/AdminSlice";
@@ -100,10 +100,9 @@ const NewProduct = () => {
         setFormData(() => {
           return {
             name: "",
-            category: "",
-            image: "",
             price: "",
             desc: "",
+            category: "",
             stock: 0,
           };
         });
