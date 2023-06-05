97f508fc128ac0bce824aa1a1ffe533d461d152b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-05 11:27:21 +0100

    Add users table to display users

--- src/admin/UsersList.js
+++ src/admin/UsersList.js
@@ -1,15 +1,55 @@
 import React,{ useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchUsers } from '../features/AdminSlice'
+import { Link } from "react-router-dom";
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
+import Loader from '../components/layout/Loader'
+import Head from '../components/layout/Head';
 
 const UsersList = () => {
-	const dispatch = useDispatch()
+	const { users, isLoading } = useSelector(state => state.admin)
+	const dispatch = useDispatch();
+
 	useEffect(() => {
 		dispatch(fetchUsers())
 	},[dispatch])
 	return (
 		<div>
-			Im the new user
+			{isLoading ? <Loader /> 
+			: 
+			<>
+			<Head title={'Users list page'}/>
+				<h1>Welcome to the All Users page</h1>
+
+				<table className="table-auto">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Role</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users?.map(user => (
+							<tr key={user._id}>
+								<td>{user._id}</td>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+								<td>{user.role}</td>
+								<td>
+									<Link to={`/admin/product/${user._id}`} className='py-1 px-2'>
+										<FaPencilAlt />
+									</Link>
+									<button className='p-1 ml-4'><FaTrash /></button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</>
+			}
 		</div>
 	)
 }
