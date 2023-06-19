ce59422f875ccdc2de3b65d19121b7b155b5cf34
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-19 23:44:17 +0100

    Remove the unused code

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/ProductSlice";
 import userReducer from "./features/UserSlice";
 import orderReducer from "./features/OrderSlice";
-import { productsApi } from './features/ProductApi';
 import cartReducer from './features/CartSlice';
 import adminReducer from './features/AdminSlice';
 
@@ -13,10 +12,7 @@ export const store = configureStore({
     cart: cartReducer,
     order: orderReducer,
     admin: adminReducer,
-    [productsApi.reducerPath]: productsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 // store.dispatch(fetchProducts())
