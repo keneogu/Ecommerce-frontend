faff1f21a4d93e7b482094cce231ffca23530d32
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-23 23:57:39 +0100

    Add footer stylings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<div>
-			<footer>
-				<p className='text-center'>Kenz-Shopping Cart @2023, All rights are Reserved by me</p>
+		<>
+			<footer className='flex w-full p-6 mt-6 justify-center bg-slate-950 relative before:content-[" "] before:absolute before:top-2 before:bottom-0 before:left-40 before:bg-slate-700 before:w-6 before:skew-y-[30deg] before:skew-x-[30deg] before:h-inherit before:origin-top after:content-[" "] after:absolute after:top-2 after:bottom-0 after:right-40 after:bg-slate-700 after:w-6 after:-skew-y-[30deg] after:-skew-x-[30deg] after:h-inherit after:origin-top'>
+				<p className='text-center text-slate-100 z-10'>Kenz-Shopping Cart @2023, All rights are Reserved by me</p>
 			</footer>
-		</div>
+		</>
 	)
 }
 
