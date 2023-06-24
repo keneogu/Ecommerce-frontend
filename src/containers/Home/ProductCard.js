0d17cce9c5ebea49b5bca43143d23e60d3588508
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-24 17:16:42 +0100

    Add card styling making equal cards

--- src/containers/Home/ProductCard.js
+++ src/containers/Home/ProductCard.js
@@ -4,24 +4,25 @@ import Rating from "../../components/Rating";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="flex flex-wrap -mx-4">
-      <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
-        <Link
-          to={`/product/${product._id}`}
-          className="block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden"
-        >
-          <div className="relative pb-56 overflow-hidden">
-            <img
-              className="absolute inset-0 h-full w-full object-contain"
-              src={product.images[0].url}
-              alt=""
-            />
-          </div>
+    <>
+      <Link
+        to={`/product/${product._id}`}
+        className="block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden"
+      >
+        <div>
+        <div className="relative pb-56 overflow-hidden">
+          <img
+            className="absolute inset-0 h-full w-full object-contain p-2"
+            src={product.images[0].url}
+            alt=""
+          />
+        </div>
+        <div>
           <div className="p-2">
             <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
               {product.category}
             </span>
-            <h2 className="mt-2 mb-2  font-bold">{product.name}</h2>
+            <h2 className="mt-2 mb-2 font-semibold">{product.name.length < 60 ? product.name : product.name.substr(0, 60) + "..."}</h2>
             <p className="text-sm">
               {product.desc.length < 60
                 ? product.desc
@@ -35,12 +36,13 @@ const ProductCard = ({ product }) => {
           </div>
           <div className="p-2 flex items-center text-sm text-gray-600">
             <Rating value={product.ratings} />
-            <p>{product.ratings}</p>
+            <p className="pl-2">{product.ratings}</p>
           </div>
           <span className="ml-2">{product.numOfReviews} Reviews</span>
-        </Link>
-      </div>
-    </div>
+        </div>
+        </div>
+      </Link>
+    </>
   );
 };
 
