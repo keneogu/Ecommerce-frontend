6cbbcf2cf3d29721b8aef7d14521164cea7ab031
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-25 23:18:44 +0100

    Add styling for forgot password page

--- src/containers/User/ForgotPassword.js
+++ src/containers/User/ForgotPassword.js
@@ -26,20 +26,36 @@ const ForgotPassword = () => {
   };
   return (
     <>
-      <Head title={"Update User"} />
-      <div>
-        <form onSubmit={handleSubmit}>
-          <h1>Forgot Password</h1>
-          <div>
-            <label htmlFor="email">Password</label>
+      <Head title={"Update Password"} />
+      <div className="flex flex-col w-full my-36 md:w-3/6 lg:w-2/6 md:my-32 md:mx-auto bg-white shadow-md hover:shadow-xl rounded-lg">
+        <form onSubmit={handleSubmit} className="flex flex-col p-4">
+          <div className="flex py-5 font-bold">
+            <h3 className="text-xl">Forgot Password</h3>
+            <p className="pt-2 px-2">
+              <FaSignInAlt />
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="email"
+              className="after:content-['*'] after:ml-0.5 block"
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
               placeholder="Email..."
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="h-8 outline-0"
             />
-            <button disabled={isLoading ? true : false}>Send Email</button>
+            <button
+              disabled={isLoading ? true : false}
+              className="bg-slate-800 text-white mx-5 my-4 p-3 rounded-md font-bold hover:bg-slate-700"
+            >
+              Send Email
+            </button>
           </div>
         </form>
       </div>
