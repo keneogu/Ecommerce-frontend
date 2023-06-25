98db65523ff96e4b32f7588ec5b1bf69cc5c7876
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-25 23:19:06 +0100

    Add styling for reset password page

--- src/containers/User/NewPassword.js
+++ src/containers/User/NewPassword.js
@@ -30,34 +30,41 @@ const NewPassword = () => {
     dispatch(resetPassword(token, data));
   };
 	return (
-		<div>
-			<Head title={"Update User"} />
+		<>
+			<Head title={"Reset Password"} />
 
-			<div>
-			<form onSubmit={handleSubmit}>
-          <h1>Reset Password</h1>
-          <div>
-            <label htmlFor="pass">Password</label>
+			<div className="flex flex-col w-full my-36 md:w-3/6 lg:w-2/6 md:my-32 md:mx-auto bg-white shadow-md hover:shadow-xl rounded-lg">
+			<form onSubmit={handleSubmit} className="flex flex-col p-4">
+      <div className="flex py-5 font-bold">
+            <h3 className="text-xl">Reset Password</h3>
+            <p className="pt-2 px-2">
+              <FaSignInAlt />
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="pass" className="after:content-['*'] after:ml-0.5 block">Password</label>
             <input
               type="password"
               id="pass"
               placeholder="Password..."
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="h-8 outline-0"
             />
-            <label htmlFor="pass1">Password</label>
+            <label htmlFor="pass1" className="after:content-['*'] after:ml-0.5 block">Confirm Password</label>
             <input
               type="password"
               id="pass1"
               placeholder="Password..."
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+              className="h-8 outline-0"
             />
-            <button disabled={isLoading ? true : false}>Send Email</button>
+            <button disabled={isLoading ? true : false} className="bg-slate-800 text-white mx-5 my-4 p-3 rounded-md font-bold hover:bg-slate-700">Reset Password</button>
           </div>
         </form>
 			</div>
-		</div>
+		</>
 	)
 }
 
