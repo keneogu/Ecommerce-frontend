c17b8870dea64390c45fb75aac1afa3cf8bfbe5f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-27 23:38:24 +0100

    Add styling to User Profile page

--- src/containers/User/UserProfile.js
+++ src/containers/User/UserProfile.js
@@ -1,45 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Head from "../../components/layout/Head";
 import Loader from "../../components/layout/Loader";
-
+import { FaArrowAltCircleRight } from "react-icons/fa"
 
 const UserProfile = () => {
-	const { isLoading, user } = useSelector((state) => state.user);
+  const { isLoading, user } = useSelector((state) => state.user);
 
-	return (
-		<div>
-			{isLoading ? <Loader /> : (
-				<>
-					<Head title={"Profile Page"} />
-					<h2>My Profile</h2>
-					<div>
-						<div>
-							<figure>
-								<img src={user.user?.avatar.url} alt={user.user?.name} />
-							</figure>
-							<Link to="/me/update">Edit Profile</Link>
-						</div>
+  return (
+    <div className="container md:mx-auto">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Head title={"Profile Page"} />
+          <div className="md:w-full md:mx-auto flex flex-col md:justify-center md:items-center my-16 mx-4">
+            <h2 className="my-4 text-slate-700 text-4xl font-bold text-center">My Profile</h2>
+            <div>
+              <div className="text-center">
+                  <img src={user.user?.avatar.url} alt={user.user?.name} className="rounded-full border-2 border-slate-700 mx-auto"/>
+								<div className="bg-slate-700 text-white my-6 py-2 text-center">
+									<Link to="/me/update" className="text-white">Edit Profile</Link>
+								</div>
+              </div>
 
-						<div>
-							<h4>Full Name</h4>
-							<p>{user.user?.name}</p>
-
-							<h4>Email Address</h4>
-							<p>{user.user?.email}</p>
-
-							<h4>Joined On</h4>
-							<p>{String(user.user?.createdAt).substring(0, 10)}</p>
-
-							{user.user?.role !== 'admin' && <Link to="/orders/me">My Orders</Link>}
-							<Link to="/password/update">Change Password</Link>
-						</div>
-					</div>
-				</>
-			)}
-		</div>
-	)
-}
+              <div className="my-3">
+								<div className="flex flex-col md:flex-row my-3">
+									<h4 className="text-slate-700 italic pr-2 font-bold">Full Name:</h4>
+                	<p className="text-xl md:text-base">{user.user?.name}</p>
+								</div>
+                
+								<div className="flex flex-col md:flex-row my-3">
+									<h4 className="text-slate-700 italic pr-2 font-bold">Email Address:</h4>
+                	<p className="text-xl md:text-base">{user.user?.email}</p>
+								</div>
+                
+								<div className="flex flex-col md:flex-row my-3">
+									<h4 className="text-slate-700 italic pr-2 font-bold">Joined On:</h4>
+                	<p className="text-xl md:text-base">{String(user.user?.createdAt).substring(0, 10)}</p>
+								</div>
+                
+								<div className="flex flex-col my-3">
+									{user.user?.role !== "admin" && (
+                  <Link to="/orders/me" className="bg-slate-700 text-white p-3">My Orders</Link>
+                )}
+								<div className="flex text-blue-600 text-lg md:text-base">
+									<Link to="/password/update" className="mr-1"> Change Password</Link>
+									<FaArrowAltCircleRight className="mt-1"/>
+								</div>
+                
+								</div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default UserProfile;
