dde45b7e55f5654880ea4173a2f9f3785cfa1472
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-06-27 23:56:30 +0100

    Add styling to update Password page

--- src/containers/User/UserUpdatePassword.js
+++ src/containers/User/UserUpdatePassword.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { updateUserPassword } from "../../features/UserSlice";
+import { FaSignInAlt } from "react-icons/fa";
 import Head from "../../components/layout/Head";
 
 const UserUpdatePassword = () => {
-	const [oldPassword, setOldPassword] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
   const [password, setPassword] = useState("");
-	const { isLoading, isUpdated } = useSelector((state) => state.user);
+  const { isLoading, isUpdated } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-
     if (isUpdated) {
       navigate("/me");
     }
@@ -30,37 +30,53 @@ const UserUpdatePassword = () => {
     dispatch(updateUserPassword(data));
   };
 
-	return (
-		<div>
+  return (
+    <div>
       <Head title={"Update Password"} />
-      <div>
-        <form onSubmit={handleSubmit}>
-          <h1>Update Password</h1>
+      <div className="flex flex-col w-full my-36 md:w-3/6 lg:w-2/6 md:my-32 md:mx-auto bg-white shadow-md hover:shadow-xl rounded-lg">
+        <form onSubmit={handleSubmit} className="flex flex-col p-4">
+          <div className="flex py-5 font-bold">
+            <h3 className="text-xl">Update Password</h3>
+            <p className="pt-2 px-2">
+              <FaSignInAlt />
+            </p>
+          </div>
           <div>
-          <label htmlFor="old_pass">Password</label>
-            <input
-              type="password"
-              id="old_pass"
-              placeholder="OldPassword"
-              name="oldPassword"
-              value={oldPassword}
-              onChange={e => setOldPassword(e.target.value)}
-            />
-            <label htmlFor="new_pass">Password</label>
-            <input
-              type="password"
-              id="new_pass"
-              placeholder="NewPassword"
-              name="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-            <button disabled={isLoading ? true : false}>Update Password</button>
+            <div className="flex flex-col">
+              <label htmlFor="old_pass" className="after:content-['*'] after:ml-0.5 block text-xl md:text-base italic">Old_Password</label>
+              <input
+                type="password"
+                id="old_pass"
+                placeholder="OldPassword"
+                name="oldPassword"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+                className="h-8 outline-0 text-xl md:text-base"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="new_pass" className="after:content-['*'] after:ml-0.5 block text-xl md:text-base italic">Password</label>
+              <input
+                type="password"
+                id="new_pass"
+                placeholder="NewPassword"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="h-8 outline-0 text-xl md:text-base"
+              />
+            </div>
+            <button
+              disabled={isLoading ? true : false}
+              className="bg-slate-800 text-white mx-5 my-4 p-3 rounded-md font-bold hover:bg-slate-700"
+            >
+              Update Password
+            </button>
           </div>
         </form>
       </div>
     </div>
-	)
-}
+  );
+};
 
 export default UserUpdatePassword;
