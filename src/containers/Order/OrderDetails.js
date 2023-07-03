61c1345d4b51a5b327b0872c13cb98a549fc2f34
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-03 16:13:11 +0100

    Fix the ordered details page and add styling

--- src/containers/Order/OrderDetails.js
+++ src/containers/Order/OrderDetails.js
@@ -1,76 +1,130 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import Loader from '../../components/layout/Loader';
-import { fetchOrderDetails } from '../../features/OrderSlice';
-import Head from '../../components/layout/Head';
+import Loader from "../../components/layout/Loader";
+import { fetchOrderDetails } from "../../features/OrderSlice";
+import Head from "../../components/layout/Head";
 
 const OrderDetails = () => {
-	const { id } = useParams();
-	const { isLoading, orderDetail } = useSelector(state => state.order);
-	console.log(orderDetail)
+  const { id } = useParams();
+  const { isLoading, orderDetail } = useSelector((state) => state.order);
+  console.log(orderDetail);
 
-	const {shippingInfo, orderedItems, paymentInfo, user, totalPrice, orderStatus} = orderDetail.cart;
-	const dispatch = useDispatch();
+  const {
+    shippingInfo,
+    orderedItems,
+    paymentInfo,
+    user,
+    totalPrice,
+    orderStatus,
+  } = orderDetail.cart;
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(fetchOrderDetails(id))
-	},[dispatch,id]);
+  useEffect(() => {
+    dispatch(fetchOrderDetails(id));
+  }, [dispatch, id]);
 
-	const shippingDetails = shippingInfo && `${shippingInfo.address}, ${shippingInfo.city}, ${shippingInfo.postalCode}, ${shippingInfo.country}`
+  const shippingDetails =
+    shippingInfo &&
+    `${shippingInfo.address}, ${shippingInfo.city}, ${shippingInfo.postalCode}, ${shippingInfo.country}`;
 
-	const isPaid = paymentInfo && paymentInfo.status === 'succeeded' ? true : false
+  const isPaid =
+    paymentInfo && paymentInfo.status === "succeeded" ? true : false;
 
-	return (
-		<div>
-			<Head title={'order Details'} />
+  return (
+    <div className="container mx-auto">
+      <Head title={"order Details"} />
 
-			{isLoading ? <Loader /> : (
-				<div>
-					<div>
-						<h1>{orderDetail.cart._id}</h1>
-						<h4>The Shipping Info:</h4>
-						<p><b>Name:</b>{user && user.name}</p>
-						<p><b>Phone:</b>{shippingInfo && shippingInfo.phoneNo}</p>
-						<p><b>Address:</b>{shippingDetails}</p>
-						<p><b>Amount:</b>{totalPrice}</p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col my-5 mx-4">
+          <h6 className="text-sm underline underline-offset-8 text-blue-500 mt-3">
+            {orderDetail.cart._id}
+          </h6>
+          <div className="my-3">
+            <h4 className="text-center text-slate-700 mb-3 font-extrabold text-3xl">
+              My Shipping Info:
+            </h4>
+            <div className="my-3">
+              <p>
+                <b>Name: </b>
+                {user && user.name}
+              </p>
+              <p>
+                <b>Phone: </b>
+                {shippingInfo && shippingInfo.phoneNo}
+              </p>
+              <p>
+                <b>Address: </b>
+                {shippingDetails}
+              </p>
+              <p>
+                <b>Amount: </b>${totalPrice}
+              </p>
+            </div>
 
-						<hr />
+            <hr />
 
-						<h4>Payment</h4>
-						<p className={isPaid ? "text-green-400" : "text-red-500"}><b>{isPaid ? "PAID" : "NOT PAID"}</b></p>
+            <div className="flex my-5">
+              <div className="flex mr-5">
+                <h4 className="pr-2">Payment:</h4>
+                <p className={isPaid ? "text-green-400" : "text-red-500"}>
+                  <b>{isPaid ? "PAID" : "NOT PAID"}</b>
+                </p>
+              </div>
 
-						<h4>Order Items</h4>
-						<p className={orderDetail.orderStatus && String(orderDetail.orderStatus).includes('Delivered') ? 'text-green-500' : 'text-red-500'}><b>{orderStatus}</b></p>
-						<hr />
+              <div className="flex mr-5">
+                <h4 className="pr-2">Order Items:</h4>
+                <p
+                  className={
+                    orderDetail.orderStatus &&
+                    String(orderDetail.orderStatus).includes("Delivered")
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }
+                >
+                  <b>{orderStatus}</b>
+                </p>
+              </div>
+            </div>
 
-						<div>
-							{orderedItems && orderedItems.map(item => (
-								<div key={item.product} className="">
-									<div className="">
-											<img src={item.image} alt={item.name} height="45" width="65" />
-									</div>
+            <hr />
 
-									<div className="">
-											<Link to={`/product/${item.product}`}>{item.name}</Link>
-									</div>
+            <div className="bg-white shadow-lg p-9">
+              {orderedItems &&
+                orderedItems.map((item) => (
+                  <div
+                    key={item.product}
+                    className="flex flex-col md:grid grid-cols-5 gap-2 my-3 items-center shadow-xl p-3"
+                  >
+                    <div className="grid-span-2">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="w-24 h-24"
+                      />
+                    </div>
 
+                    <div className="grid-span-1">
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </div>
 
-									<div className="">
-											<p>${item.price}</p>
-									</div>
+                    <div className="grid-span-1">
+                      <p>${item.price}</p>
+                    </div>
 
-									<div className="">
-											<p>{item.quantity} Piece(s)</p>
-									</div>
-								</div>
-							))}
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
-	)
-}
+                    <div className="grid-span-1">
+                      <p>{item.quantity} Piece(s)</p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default OrderDetails;
