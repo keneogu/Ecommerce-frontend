78da3b3ad9e7f7ddab8f0478373d2ce2ef98b586
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-03 16:10:01 +0100

    Add styling to checkout page

--- src/containers/Cart/Checkout.js
+++ src/containers/Cart/Checkout.js
@@ -1,46 +1,59 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Checkout = ({shipping, confirmOrder, payment}) => {
-	return (
-		<div>
-			{shipping ? <Link to='/shipping'>
-				<div></div>
-				<div>Shipping</div>
-				<div></div>
-			</Link> : 
-			<Link to='#' disabled>
-				<div></div>
-				<div>Shipping</div>
-				<div></div>
-			</Link>
-			}
+const Checkout = ({ shipping, confirmOrder, payment }) => {
+  return (
+    <div className="flex container justify-center items-center mt-4">
+      {shipping ? (
+        <Link
+          to="/shipping"
+          className="mx-3 rounded-lg bg-slate-700 text-white p-3"
+        >
+          <div></div>
+          <div>Shipping</div>
+          <div></div>
+        </Link>
+      ) : (
+        <Link
+          to="#"
+          disabled
+          className="mx-3 rounded-lg bg-slate-700 text-white p-3"
+        >
+          <div></div>
+          <div>Shipping</div>
+          <div></div>
+        </Link>
+      )}
 
-{confirmOrder ? <Link to='#'>
-				<div></div>
-				<div>Confirm Order</div>
-				<div></div>
-			</Link> : 
-			<Link to='#' disabled>
-				<div></div>
-				<div>Confirm Order</div>
-				<div></div>
-			</Link>
-			}
+      {confirmOrder ? (
+        <Link to="#" className="mx-3 rounded-lg bg-slate-700 text-white p-3">
+          <div></div>
+          <div>Confirm Order</div>
+          <div></div>
+        </Link>
+      ) : (
+        <Link to="#" disabled className="mx-3">
+          <div></div>
+          <div>Confirm Order</div>
+          <div></div>
+        </Link>
+      )}
 
-{payment ? <Link to='#'>
-				<div></div>
-				<div>Payment</div>
-				<div></div>
-			</Link> : 
-			<Link to='#' disabled>
-				<div></div>
-				<div>Payment</div>
-				<div></div>
-			</Link>
-			}
-		</div>
-	)
-}
+      {payment ? (
+        <Link to="#" className="mx-3 rounded-lg bg-slate-700 text-white p-3">
+          <div></div>
+          <div>Payment</div>
+          <div></div>
+        </Link>
+      ) : (
+        <Link to="#" disabled className="mx-3">
+          <div></div>
+          <div>Payment</div>
+          <div></div>
+        </Link>
+      )}
+    </div>
+  );
+};
 
-export default Checkout
+export default Checkout;
